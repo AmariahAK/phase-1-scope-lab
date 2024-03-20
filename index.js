c599cc7be9e4a6d282f1769cf6c7d0deedef1849
Amariah <amariah.abish@gmail.com>
2024-03-20 16:31:59 +0300

    JS Scope Lab

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var customerName = 'bob';
 
 // Write a function that accesses that global customerName variable, and uppercases it.
 function upperCaseCustomerName() {
-    // Create a new variable to hold the uppercase version of customerName
-    var upperCaseName = customerName.toUpperCase();
-    return upperCaseName;
+    return customerName.toUpperCase(); // Return the uppercase version without modifying the original variable
 }
 
 // Write a function that when called, declares a variable called bestCustomer in global scope and assigns it to be 'not bob'.
